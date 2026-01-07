47e0aa3f6efbc8aa38a84c4cda1315b91a5fe8e3
smit1999 <smitmvora@gmail.com>
2026-01-07 14:05:21 -0500

    printing changes

--- controller/errorController.js
+++ controller/errorController.js
@@ -1,6 +1,6 @@
 exports.errformat = (err,req,res,next)=>{
     err.statusCode = err.statusCode || 500;
-    err.status = err.status || 'error something went wrong'
+    err.status = err.status || 'error! something went wrong'
     res.status(err.statusCode).json({
         status:err.status,
         message: err.message,
